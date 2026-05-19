d01e5aa6b0d5c0620f86709f65ee5a623e77294e
Dev-Jhuvel <devjhuvel@gmail.com>
2026-05-19 08:06:31 +0800

    adjust product card

--- src/components/ProductCardOld.jsx
+++ src/components/ProductCardOld.jsx
@@ -1,36 +1,40 @@
 import ramen from "../images/products/ramen.png";
 
 export default function ProductCard({ product, once = false }) {
+  const fontSize = product.name.length > 17 ? "sm:text-sm" : "sm:text-lg";
+
   return (
     <div
       id="card"
-      className="border bg-black w-full rounded-2xl transition-transform duration-500 ease-in-out hover:scale-106 shadow-2xl hover:shadow-[0_0_15px_rgba(255,255,255,0.4)] hover:bg-gray-500/50 relative max-w-76 max-h-90 group"
-      // data-aos="fade-right"
+      className="border bg-black w-full p-3 pt-1 rounded-2xl transition-transform duration-500 ease-in-out hover:scale-106 shadow-2xl hover:shadow-[0_0_15px_rgba(255,255,255,0.4)] hover:bg-gray-500/50 relative"
+      data-aos="fade-right"
       data-aos-once={once}
       data-aos-delay={500}
     >
       {product.badge && (
-        <div className="absolute bg-red-500 rounded-full size-14 left-15 top-2 border-t-2 z-2">
+        <div className="absolute bg-red-500 rounded-full size-14 right-[-5px] top-[-4px] border-t-2">
           <span className="text-[7pt] sm:text-[8pt] font-semibold size-full px-1 text-center flex justify-center items-center">
             {product.badge}
           </span>
         </div>
       )}
-      <div className="flex justify-center border relative overflow-hidden">
-        <figure className="aspect-[4/3] overflow-hidden">
+      <div className="flex justify-center">
+        <figure  className="aspect-[4/3] overflow-hidden">
           <img
-            className="size-full max-w-50 object-cover object-center"
+            className="size-full md:max-w-55"
             src={product.image ?? ramen}
             alt="product-image"
           />
         </figure>
       </div>
-      <div className="flex flex-col px-4 py-2">
-        <div className="text-left flex flex-col gap-1">
-          <h3 className={`text-lg sm:text-lg font-bold leading-tight`}>
+      <div className="flex flex-col h-auto sm:h-45">
+        <div className="text-left flex justify-between items-center h-13 sm:h-auto">
+          <h3 className={`text-lg sm:text-xl font-bold leading-tight`}>
             {product.name}
           </h3>
-          <p className="text-xs sm:text-[12px] leading-tight text-gray-300 line-clamp-3">
+        </div>
+        <div className="py-2 h-24 sm:h-auto">
+          <p className="text-sm sm:text-base leading-tight text-gray-300 line-clamp-3">
             {product.description}
           </p>
         </div>
@@ -42,7 +46,7 @@ export default function ProductCard({ product, once = false }) {
                 return (
                   <span
                     key={key}
-                    className={`text-[11px] ${colorMap[key]} px-2 py-1 rounded-2xl`}
+                    className={`text-xs ${colorMap[key]} px-2 py-1 rounded-2xl`}
                   >
                     {tag}
                   </span>
@@ -50,7 +54,9 @@ export default function ProductCard({ product, once = false }) {
               })}
           </div>
           <div className="mt-3">
-            
+            <span className="block mt-3 text-sm sm:text-base font-semibold">
+              ${product.price}
+            </span>
           </div>
         </div>
       </div>
--- src/sections/Contacts.jsx
+++ src/sections/Contacts.jsx
@@ -9,11 +9,11 @@ export default function Contacts() {
       <div className="container mx-auto px-4 py-5">
         <h1 className="text-center text-lg">Contact Us</h1>
         <div className="grid grid-cols-1 lg:grid-cols-4 text-center lg:text-left ">
-          <div className="p-4 mx-auto">
+          <div className="p-4">
             <h3 className="text-3xl font-semibold">Visit our Restaurant</h3>
-            <a className="text-lg lg:pr-20" href={restaurant.gmap} target="_blank">{restaurant.address}</a>
+            <a className="text-lg lg:pr-5" href={restaurant.gmap} target="_blank">{restaurant.address}</a>
           </div>
-          <div className="p-4 mx-auto">
+          <div className="p-4">
             <h3 className="text-3xl font-semibold mb-3">Contact Us</h3>
             <div className="flex justify-center lg:justify-start items-center gap-3 mb-2">
               <PhoneCall /> <a href={`tel:${phone_number}`} target="_blank">{restaurant.phone_number}</a>
@@ -22,7 +22,7 @@ export default function Contacts() {
               <MailIcon /> <a href={`mailto:${restaurant.email}`} target="_blank"> {restaurant.email}</a>
             </div>
           </div>
-          <div className="p-4 mx-auto">
+          <div className="p-4">
             <h3 className="text-3xl font-semibold mb-3">Store Hours</h3>
             <div className="mb-3">
               <p className="text-lg/3"> Weekdays</p>
@@ -42,7 +42,7 @@ export default function Contacts() {
             </div>
           </div>
         </div>
-        <div className="h-30 flex items-center justify-center">
+        <div className="h-30 flex items-center justify-center py-5">
            <h3
             className="font-bold text-center text-white text-[clamp(80px,15vw,350px)] text-shadow-md text-shadow-black my-2"
             // data-aos="fade-up"
